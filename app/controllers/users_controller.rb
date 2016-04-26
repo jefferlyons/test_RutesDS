a8b32dc5e38741aab5c52961e3431b3f6a23483d
jefferlyons <jefferlyons@outlook.com>
2016-04-25 19:23:38 -0500

    double fuck u jquery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
